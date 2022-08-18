bcec2aa5c8e43a2c3c14695811586ad95c72dd57
Semih Demir <joinko77@gmail.com>
2022-08-18 14:53:20 +0300

    18-08-22

---
+++ src/Q01_Scanner/S01.java
@@ -0,0 +1,10 @@
+package Q01_Scanner;
+
+public class S01 {
+
+    public static void main(String[] args) {
+        /*
+        Kullanıcıdan bir sayı alın ve sayının küpünün yarısını konsola yazdıran bir program yazın.
+         */
+    }
+}
