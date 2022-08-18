c9327a43a94279f64067958873c3677b71f0ef2d
Semih Demir <joinko77@gmail.com>
2022-08-18 14:59:03 +0300

    18-08-22

--- src/Q01_Scanner/S01.java
+++ src/Q01_Scanner/S01.java
@@ -1,10 +1,20 @@
 package Q01_Scanner;
 
+import jdk.swing.interop.SwingInterOpUtils;
+
+import java.util.Scanner;
+
 public class S01 {
 
     public static void main(String[] args) {
         /*
         Kullanıcıdan bir sayı alın ve sayının küpünün yarısını konsola yazdıran bir program yazın.
          */
+
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Lutfen bir sayi giriniz");
+        double sayi=scan.nextInt();
+
+        System.out.println(sayi*sayi*sayi/2);
     }
 }
